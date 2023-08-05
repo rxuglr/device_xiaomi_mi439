1a5afa90fd741de0acbde2c3db3624b79b06d1dc
rxuglr <rxuglr@mail.ru>
2023-08-05 17:28:06 +0300

    mi439: Add Framework Compatibility Matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,9 @@ BOARD_SHIPPING_API_LEVEL := 30
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    vendor/lineage/config/device_framework_matrix.xml \
+    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest_k$(TARGET_KERNEL_VERSION).xml
 
